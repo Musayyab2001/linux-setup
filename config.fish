e19a67cc18d8afc70442724c5138490784e77c03
Musayyab Naveed <mussayyab201@gmail.com>
2020-03-21 22:23:18 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ GNU nano 4.8             /home/musayyab/.config/fish/config.fish
 set -gx PATH ~/.npm-global/bin $PATH
 
 set --export ANDROID $HOME/Android;
-set --export JAVA_HOME /usr/local/java/jdk1.8.0_241/;
+set --export JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64;
 set --export ANDROID_HOME $ANDROID/Sdk;
 set -gx PATH $ANDROID_HOME/tools $PATH;
 set -gx PATH $ANDROID_HOME/tools/bin $PATH;
