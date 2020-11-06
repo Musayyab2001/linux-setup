84160344ebb90aff0d8c4f674f70549fb373b45f
Musayyab Naveed <mussayyab201@gmail.com>
2020-11-06 18:25:28 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,8 @@
-GNU nano 4.8             /home/musayyab/.config/fish/config.fish
-
 # see nvm setup
 # set -gx PATH ~/.npm-global/bin $PATH
 
 set --export ANDROID $HOME/Android;
-set --export JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64;
+set --export JAVA_HOME /usr/lib/jvm/java-14-openjdk-amd64;
 set --export ANDROID_HOME $ANDROID/Sdk;
 set -gx PATH $ANDROID_HOME/tools $PATH;
 set -gx PATH $ANDROID_HOME/tools/bin $PATH;
@@ -21,3 +19,12 @@ end
 
 set -x NVM_DIR ~/.nvm
 nvm use default --silent
+
+
+function grm
+    git checkout $argv && git rebase master && git push -f && git checkout master && git merge $argv && git push
+end
+
+function gdlo
+    git push --delete origin $argv && git branch -D $argv
+end
